b9f802a885a8be3252081f0dc7fc8decf55177f2
Owen <poonhauwang6473@gmail.com>
2022-02-07 21:41:59 +0800

    21:41 Source

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ 
+    //Carousel 
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 5,
+    });
+
+});
+  
