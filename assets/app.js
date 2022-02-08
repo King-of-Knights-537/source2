89d918d6c5367720e54e89e1b854ef433d892644
Owen <poonhauwang6473@gmail.com>
2022-02-08 21:18:31 +0800

    21:18 Source

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+    //To Show Hidden Color of Navbar
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
  
     //Carousel 
     $('.flexslider').flexslider({
@@ -7,6 +13,20 @@ $(document).ready(function() {
         itemWidth: 210,
         itemMargin: 5,
     });
+    
+    //Scroll-Animation
+    const r = rolly({
+        view: document.querySelector('.app'),
+        native: true,
+        // other options
+    });
+    r.init();
+
+    AOS.init({
+        duration: 1500 // global duration
+    });
 
 });
+
+
   
